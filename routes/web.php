b5b6bf3a7ad4e75f8b8845c9a9908684d7d4dfd2
kiyomin0615 <moeday1010@gmail.com>
2024-08-08 00:52:43 +0900

    add first routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return '<h1>Home</h1><a href="/about">About 페이지 보기</a>';
+});
+
+Route::get("/about", function () {
+    return '<h1>About</h1><a href="/">Home 페이지로 돌아가기</a>';
 });
