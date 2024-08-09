1e1474c7182e052712ffd5c58caa59873c688186
kiyomin0615 <moeday1010@gmail.com>
2024-08-09 10:02:07 +0900

    validate user input data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,18 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $req)
     {
+        // Validate user input data
         $userInput = $req->validate([
-            'username' => 'required',
-            'email' => 'required',
-            'password' => 'required'
+            'username' => ['required', 'min:5', 'max:15', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:8', 'max:20', 'confirmed'], // 'password_confimation' field is required on form data
         ]);
 
+        // Hash password
+        $userInput['password'] = bcrypt($userInput['password']);
+
         User::create($userInput);
+
         return 'User Registration!';
     }
 }
